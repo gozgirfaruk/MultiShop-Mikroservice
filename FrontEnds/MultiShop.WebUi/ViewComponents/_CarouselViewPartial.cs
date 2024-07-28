fb121d7e46891320e7f4cef35b7a147d21af05a6
gozgirfaruk <gozgirfaruk@gmail.com>
2024-07-28 18:33:25 +0300

    Vitrin teması eklendi.

---
+++ FrontEnds/MultiShop.WebUi/ViewComponents/_CarouselViewPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUi.ViewComponents
+{
+    public class _CarouselViewPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FrontEnds/MultiShop.WebUi/ViewComponents/_FeatureProductsViewPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUi.ViewComponents
+{
+    public class _FeatureProductsViewPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
